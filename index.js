67dd9b3f87869b2d67f5d5f8baddcc064c1f5570
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-24 20:07:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, str) {
+  drivers.filter (function(driver){return (driver === str || driver.toLowerCase() === str)});
+}
+
+function fuzzyMatch(drivers, str) {
+  drivers.filter (function(driver) {return driver.startsWith(str) });
+}
+
+function matchName (drivers, str) {
+  drivers.filter(function(driver) {return driver.name === str } );
+}
